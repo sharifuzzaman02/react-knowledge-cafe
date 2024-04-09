96ccffec9390b029a0af236fc0da9257fd7ea498
Sharifuzzaman Sumon <sumon37070@gmail.com>
2024-04-09 10:23:36 +0600

    reading time added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Blog from "../Blog/Blog";
+import PropTypes from 'prop-types'
 
-const Blogs = () => {
+const Blogs = ({handleAddToBookmark, handleMarkAsRead}) => {
     const [blogs, setBlogs] = useState([]);
 
     useEffect( () =>{
@@ -15,13 +16,21 @@ const Blogs = () => {
         <div className="md:w-2/3">
             <h1 className="text 4xl">Blogs: {blogs.length} </h1>
             {
-                blogs.map(blog => <Blog 
+                blogs.map(blog => <Blog
                     key={blog.id} 
                     blog={blog}
+                    handleAddToBookmark = {handleAddToBookmark}
+                    handleMarkAsRead = {handleMarkAsRead}
                 ></Blog>)
             }
         </div>
     );
 };
+
+
+Blogs.propTypes = {
+    handleAddToBookmark: PropTypes.func,
+    handleMarkAsRead: PropTypes.func
+}
 
 export default Blogs;
